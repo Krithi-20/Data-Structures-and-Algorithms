777daf492f10dc0e6957ee1624c0d3aa4c34383b
Krithi <krithi.edu@gmail.com>
2026-01-19 11:40:20 +0530

    Strivers Easy Arrays

---
+++ LongestSubarrayWithSumKNegatives.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestSubarray(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0, maxLen = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (sum == k) {
+                maxLen = i + 1;
+            }
+
+            if (map.containsKey(sum - k)) {
+                maxLen = Math.max(maxLen, i - map.get(sum - k));
+            }
+
+            // store first occurrence only
+            map.putIfAbsent(sum, i);
+        }
+
+        return maxLen;
+    }
+}
---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,12 @@
+class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int i=0, n= nums.length;
+        for(int j=1; j<n; j++) {
+            if(nums[i] != nums[j]) {
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+}
