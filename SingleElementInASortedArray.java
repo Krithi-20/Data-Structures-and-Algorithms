b96c9fc43db072dd2d1f545dad93c6a29fd9f515
Krithi <krithi.edu@gmail.com>
2026-01-29 23:56:53 +0530

    29-01-2026

---
+++ SingleElementInASortedArray.java
@@ -0,0 +1,29 @@
+class SingleElementInASortedArray {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+
+        if(nums.length == 1) return nums[0];
+        if(nums[0] !=  nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+        int low = 1;
+        int high = n-2;
+
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+
+            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]) {
+                return nums[mid];
+            }
+
+            if((mid%2 == 1 && nums[mid-1] == nums[mid]) || (mid%2 == 0 && nums[mid] == nums[mid+1])) { //eleiminate left half
+                low = mid+1;
+            }
+
+            else {
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
